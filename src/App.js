356a3bee2caf037bbb72a0eea597962318c023b2
BrysonWilks <32720810+BrysonWilks@users.noreply.github.com>
2021-07-20 07:04:47 -0700

    updating state

--- src/App.js
+++ src/App.js
@@ -1,19 +1,40 @@
 //import logo from './logo.svg';
 import './App.css';
 import Info from './Info.js';
+import { useState } from 'react';
 
+/* Passing in props */
 function App() {
   return (
     <div className="App">
       <p> Barebones react app </p>
       <Info title = "Inventory Application"></Info>
-      <AddItem text ="Bryson" number = {2}></AddItem> // Passing in props
-      <AddItem></AddItem>
-      <AddItem></AddItem>
+      <ButtonState />
     </div>
   );
 }
+function ButtonState() {
+const [title, setTitle] = useState("");
+const [count, setCount] = useState(0);
 
+const updateTitleClicked = () => {
+  setTitle("You have a title now! :) ");
+}
+
+const updateCountClicked = () =>{
+  setCount(count + 1);
+}
+
+    return(
+      <div>
+      <p> Title: {title} </p>
+      <p> Counter: {count} </p>
+      <button onClick = {updateTitleClicked}> Update Title </button>
+      <button onClick = {updateCountClicked}> Update Counter </button>
+      </div>
+    );
+}
+/*
 function AddItem(props){
   const value = props.text;
   return(
@@ -24,8 +45,9 @@ function AddItem(props){
     </form>
   );
 }
-AddItem.defaultProps = { // creating defaultProps
+AddItem.defaultProps = {  creating defaultProps
   number: 0,
   text: "Nada"
 }
+*/
 export default App;
